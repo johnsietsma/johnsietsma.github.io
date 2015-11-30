688bace7167fef6b3976b93a4583b3da0367708b
John Sietsma <john.sietsma@gmail.com>
2015-11-30 11:16:48 +1100

    Remove Bundler/Gemfile, only one gem to install.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env bash
 
-add-apt-repository ppa:brightbox/ruby-ng
+add-apt-repository ppa:brightbox/ruby-ng # For ruby2.1
 apt-get update
-
-apt-get -y install zlibc zlib1g-dev zlib1g # Dep of bundler/nokogiri
 
 # Make sure we're on ruby 2
 apt-get -y install ruby-switch
 ruby-switch --set ruby2.1
 apt-get -y install ruby2.1 ruby2.1-dev # required for Jekyll
 
-gem install bundler # Install all the gems in Gemfile
-
-# run the bundle install in user mode
-pushd /vagrant
-sudo -u vagrant bundle install
-popd
+gem install 'github-pages'
 
 # node.js
 apt-get -y install nodejs
